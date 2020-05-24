b3a58add6dc661b17b5c9c94557aa6f49d689ce1
Arnab K Roy <arnab.developer26@gmail.com>
2020-05-24 21:24:53 +0530

    master

--- classifier_score.py
+++ classifier_score.py
@@ -1,27 +1,28 @@
 import pandas as pd
 import numpy as np
-import os
-from sklearn.metrics import classification_report
-from sklearn.metrics import roc_curve , log_loss,roc_auc_score, confusion_matrix
 from sklearn.model_selection import cross_val_score
-from sklearn.model_selection import train_test_split
 import warnings
 warnings.filterwarnings("ignore")
-from sklearn.model_selection import GridSearchCV
 
 def importData(path):
     df = pd.read_csv(path)
     return df
 
-def scoreModel(model,X,y):       
-    cv_score_ll = abs(cross_val_score(model, X, y, scoring='neg_log_loss', cv=10).mean())
-    cv_score_f1 = abs(cross_val_score(model, X, y, scoring='f1', cv=10).mean())
-    cv_score_ac = abs(cross_val_score(model, X, y, scoring='accuracy', cv=10).mean())
-    cv_score_rc = abs(cross_val_score(model, X, y, scoring='recall', cv=10).mean())
-    cv_score_pr = abs(cross_val_score(model, X, y, scoring='precision', cv=10).mean())
-    cv_score_roc = abs(cross_val_score(model, X, y, scoring='roc_auc', cv=10).mean())
+def scoreModel(models,X,y):
+    score_df = pd.DataFrame(columns=['Recall','Precision','F1-Score','Accuracy','ROC','LogLoss'])
+    for model in models:       
+        ll = abs(cross_val_score(model, X, y, scoring='neg_log_loss', cv=10).mean())
+        f1 = abs(cross_val_score(model, X, y, scoring='f1', cv=10).mean())
+        ac = abs(cross_val_score(model, X, y, scoring='accuracy', cv=10).mean())
+        rc = abs(cross_val_score(model, X, y, scoring='recall', cv=10).mean())
+        pr = abs(cross_val_score(model, X, y, scoring='precision', cv=10).mean())
+        roc = abs(cross_val_score(model, X, y, scoring='roc_auc', cv=10).mean())
 
-    return cv_score_ll, cv_score_f1, cv_score_ac, cv_score_rc, cv_score_pr, cv_score_roc
+        score_dct = {'Model':[str(model)],'Recall':[rc],'Precision':[pr],'F1-Score':[f1],'Accuracy':[ac],'ROC':[roc],'LogLoss':[ll]}
+        temp = pd.DataFrame(data = score_dct)
+        score_df = pd.concat([score_df,temp])
+
+    return score_df
 
 
 
--- main.py
+++ main.py
@@ -1,6 +1,14 @@
 from classifier_score import importData
 from classifier_score import scoreModel
+from sklearn.naive_bayes import GaussianNB
 
+path = 'D:\\DataScience\\MachineHack_FinRisk\\Financial_Risk_Participants_Data\\Financial_Risk_Participants_Data\\Train.csv'
+df = importData(path)
 
-df = importData('D:\DataScience\MachineHack_FinRisk\Financial_Risk_Participants_Data\Financial_Risk_Participants_Data\Train.csv')
-print(df.head())
+var = ['Location_Score','Internal_Audit_Score','External_Audit_Score','Fin_Score']
+X = df[var]
+y = df['IsUnderRisk']
+model = GaussianNB()
+score_df = scoreModel([model],X,y)
+
+print(score_df)
