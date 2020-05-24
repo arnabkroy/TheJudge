bdf6afe483f020bd70d8f48375eef213547e8a46
Arnab K Roy <arnab.developer26@gmail.com>
2020-05-24 21:59:17 +0530

    master

--- classifier_score.py
+++ classifier_score.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import numpy as np
 from sklearn.model_selection import cross_val_score
+import matplotlib.pyplot as plt
+import seaborn as sns
 import warnings
 warnings.filterwarnings("ignore")
 
@@ -10,7 +12,9 @@ def importData(path):
 
 def scoreModel(models,X,y):
     score_df = pd.DataFrame(columns=['Recall','Precision','F1-Score','Accuracy','ROC','LogLoss'])
-    for model in models:       
+    for model in models:
+        pos = str(model).find('(')
+        name = str(model)[:pos]       
         ll = abs(cross_val_score(model, X, y, scoring='neg_log_loss', cv=10).mean())
         f1 = abs(cross_val_score(model, X, y, scoring='f1', cv=10).mean())
         ac = abs(cross_val_score(model, X, y, scoring='accuracy', cv=10).mean())
@@ -18,13 +22,23 @@ def scoreModel(models,X,y):
         pr = abs(cross_val_score(model, X, y, scoring='precision', cv=10).mean())
         roc = abs(cross_val_score(model, X, y, scoring='roc_auc', cv=10).mean())
 
-        score_dct = {'Model':[str(model)],'Recall':[rc],'Precision':[pr],'F1-Score':[f1],'Accuracy':[ac],'ROC':[roc],'LogLoss':[ll]}
+        score_dct = {'Model':[name],'Recall':[rc],'Precision':[pr],'F1-Score':[f1],'Accuracy':[ac],'ROC':[roc],'LogLoss':[ll]}
         temp = pd.DataFrame(data = score_dct)
         score_df = pd.concat([score_df,temp])
 
     return score_df
 
-
-
+def plotScores(score_df):
+    figure = plt.figure(figsize=(15,8))
+    plt.plot(score_df['Model'],score_df['Recall'],color='orange',marker='o')
+    plt.plot(score_df['Model'],score_df['Precision'],color='blue',marker='o')
+    plt.plot(score_df['Model'],score_df['F1-Score'],color='green',marker='o')
+    plt.plot(score_df['Model'],score_df['Accuracy'],color='magenta',marker='o')
+    plt.plot(score_df['Model'],score_df['ROC'],color='red',marker='o')
+    plt.plot(score_df['Model'],score_df['LogLoss'],color='grey',marker='o')
+    plt.legend(['Recall','Precision','F1-Score','Accuracy','ROC','LogLoss'])
+    plt.ylabel('Score')
+    plt.xlabel('Models')
+    plt.show()
 
     
--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-from classifier_score import importData
+from classifier_score import importData, scoreModel, plotScores
 from classifier_score import scoreModel
 from sklearn.naive_bayes import GaussianNB
+from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier
+from sklearn.ensemble import GradientBoostingClassifier
 
 path = 'D:\\DataScience\\MachineHack_FinRisk\\Financial_Risk_Participants_Data\\Financial_Risk_Participants_Data\\Train.csv'
 df = importData(path)
@@ -8,7 +11,8 @@ df = importData(path)
 var = ['Location_Score','Internal_Audit_Score','External_Audit_Score','Fin_Score']
 X = df[var]
 y = df['IsUnderRisk']
-model = GaussianNB()
-score_df = scoreModel([model],X,y)
+classifiers = [LogisticRegression(random_state=100),RandomForestClassifier(random_state=100),GaussianNB(),AdaBoostClassifier(random_state=100),GradientBoostingClassifier(random_state=100)]
+score_df = scoreModel(classifiers,X,y)
 
 print(score_df)
+plotScores(score_df)
